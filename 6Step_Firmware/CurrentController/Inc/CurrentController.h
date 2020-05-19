11df5dccc9f6c75d53fd610d4979e9ad99b165dc
RKIyer <rahul.iyer2@gmail.com>
2020-05-19 11:46:30 -0700

    fixed point current controller implementation...needs validation

--- 6Step_Firmware/CurrentController/Inc/CurrentController.h
+++ 6Step_Firmware/CurrentController/Inc/CurrentController.h
@@ -3,18 +3,30 @@
 
 #include <stdint.h>
 
-#define PWM_COUNTER_MAX 499.f
+#define PWM_COUNTER_MAX (100-1)
 
-#define I_CONTROL_KI 0.002f
-#define I_CONTROL_KP 60.f
+#define I_CONTROL_KP 60
+#define I_CONTROL_KI_SHIFT 5 // Ki = 0.03125
+#define VA_DUTY_SCALE_SHIFT 24 // gain of 6e-8
+
+
+/*
+CURRENT SENSE: 
+- current is sensed via ADC Channel 4
+-current samples are 12-bit resolution stored in 32-bit integer
+- therefore iref should also be a 12-bit maximum value stored in a 32-bit integer
+   - as a further requirement, iref has an upper bound value because we want to current-limit the regulated current
+- current error should be stored in 32-bit integer (difference of two 32-bit values)
+*/
+
 
 typedef struct PI_params_S
 {
-    uint32_t ierr;
-    float integ_acc;
-    float vsupply;
+    int32_t ierr;
+    int32_t integ_acc;
 } PI_params_S;
 
+void PI_I_init();
 int PI_Iloop();
 
 #endif //CURRENT_CONTROLLER_H
